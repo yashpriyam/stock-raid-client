f63374390dc03f0c658f7335d9811ebda3aa580f
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-08-27 19:13:50 +0530

    Updates

---
+++ precache-manifest.80a640a780ace78937d7bd14daf5183a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b599bba1dcb8dd1dbbd7e47c50d92455",
+    "url": "/stock-raid-client/index.html"
+  },
+  {
+    "revision": "8708051f6f30cef629af",
+    "url": "/stock-raid-client/static/css/main.407e3dad.chunk.css"
+  },
+  {
+    "revision": "8f66c3bb22be37e6e5d4",
+    "url": "/stock-raid-client/static/js/2.65d1c855.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stock-raid-client/static/js/2.65d1c855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8708051f6f30cef629af",
+    "url": "/stock-raid-client/static/js/main.43cb3293.chunk.js"
+  },
+  {
+    "revision": "db1a48a81bc846d9fc46",
+    "url": "/stock-raid-client/static/js/runtime-main.be47ff8f.js"
+  },
+  {
+    "revision": "76ed3f52231adc21d68c72e5ac25458a",
+    "url": "/stock-raid-client/static/media/pexels-paul-ijsendoorn-1181179.76ed3f52.jpg"
+  }
+]);
